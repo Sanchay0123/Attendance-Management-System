d6a40d4327743ea141c81679be7ca850ee76391f
Sanchay Jain <sanchayjain35@gmail.com>
2025-04-23 09:49:25 +0530

    Fix: build client to dist/public for Render

--- SolarSystemExplorer/server/index.ts
+++ SolarSystemExplorer/server/index.ts
@@ -56,7 +56,7 @@ app.use((req, res, next) => {
   }
 
   const port = 5000;
-  server.listen(port, "127.0.0.1", () => {
+  server.listen(port, "0.0.0.0", () => {
     log(`serving on port ${port}`);
   });
 })();
